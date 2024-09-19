ee80ff20bebe0019ca249fec2b902f320dc54395
ranya hamzaoui <ranya.hamzaoui91@gmail.com>
2024-09-19 23:58:25 +0200

    set env variable db

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  baseurl: 'http://social-media-network.domain:3000/api',
+  // baseurl: 'http://social-media-network.domain:3000/api',
+  baseurl: 'https://social-media-app-api-rh.onrender.com/api',
 };
